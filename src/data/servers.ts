e6ae73b43aa9a1728858be5091568d91d98dd019
dingtianzhu <8303849+dingtianzhu@user.noreply.gitee.com>
2025-11-27 01:38:47 +0800

    update

--- src/data/servers.ts
+++ src/data/servers.ts
@@ -10,15 +10,14 @@ export const SERVER_LIST: ServerNode[] = [
     ping: 145,
     baseConfig: `
 [Interface]
-PrivateKey = sPUIqUyJf7HT1rSofRJUd8OYFlZIfDuB+qTMKrag+3M=
+PrivateKey = 8KhqVpaQ+hdUqP6deAz/NSZfXtp30xqdakECeYR70EI=
 Address = 10.66.66.2/32
-DNS = 1.1.1.1,1.0.0.1
+DNS = 1.1.1.1
 MTU = 1280
 
 [Peer]
-PublicKey = C/21VKfLcCy7Xi8NviEvMP0o1A2jyYBooe/xeOzPi1k=
-PresharedKey = TR5FO72qLYVjRwGSeT/n3S4NH08zmj/uw7Hs50uN2qk=
-Endpoint = 47.88.55.204:62635
+PublicKey = jbjrwioxyN9k7KoQCpO3Y9me3Sg5SXRU9fvuIelF6gM=
+Endpoint = 47.88.55.204:51820
 AllowedIPs = 0.0.0.0/0
 PersistentKeepalive = 25
 `.trim(),
